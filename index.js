a0365c79baf1b5b48fe01520000985d5c90a3f97
Michaela Olson <mco44@cornell.edu>
2018-05-12 17:09:02 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Write your solution in this file!
+const driver = {
+
+
+}
+
+function updateDriverWithKeyAndValue(drive,key,value) {
+  const newDriver = {...drive};
+  newDriver[key] = value;
+//  newDriver.address = add1;
+  return newDriver;
+}
+
+function destructivelyUpdateDriverWithKeyAndValue(drive,key,value) {
+  drive[key]= value;
+  return drive;
+}
+
+function deleteFromDriverByKey(drive,key) {
+  const newDriver = {...drive};
+  delete newDriver[key];
+  return newDriver;
+}
+
+function destructivelyDeleteFromDriverByKey(drive,key) {
+  delete drive[key];
+  return drive;
+}
